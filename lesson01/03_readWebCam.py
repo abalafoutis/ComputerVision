6c99dfa1ff75c25d559ab0e0fca2e63dac3d6244
abalafoutis <abalafoutis@gmail.com>
2020-09-20 23:59:17 +0300

    lesson01 webCamy

---
+++ lesson01/03_readWebCam.py
@@ -0,0 +1,11 @@
+import cv2
+
+video = cv2.VideoCapture(0)
+video.set(3, 480) # Το id=3 αλλάζει το ύψος
+video.set(4, 640) # το id=4 αλλάζει το πλάτος
+video.set(10, 100) # το id=10 αλλάζει τη φωτεινότητα
+
+while True:
+   success, img = video.read()
+   cv2.imshow("Video", img)
+   cv2.waitKey(1)
